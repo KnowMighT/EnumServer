fa807e83c32459dcfad3a7b14516def0e9ab82bc
Desperado <731759679@qq.com>
2018-06-21 16:37:00 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,52 +1,39 @@
+
+
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
+    apps: [
+        {
+            name: 'EnumServer',
+            script: './bin/run.js',
+            env: {
+                COMMON_VARIABLE: 'true',
+            },
+            env_production: {
+                NODE_ENV: 'production'
+            }
+        }
+    ],
 
-    // First application
-    {
-      name      : 'API',
-      script    : 'app.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
-    },
+    deploy: {
+        production: {
+            user: 'Desperado',
+            host: '47.96.95.187',
+            ref: 'origin/master',
+            repo: 'git@github.com:KnowMighT/EnumServer.git',
+            path: '/home/Desperado/BimLink/PM2Deploy',
+            'post-deploy': 'npm install'
+        },
 
-    // Second application
-    {
-      name      : 'WEB',
-      script    : 'web.js'
+        dev: {
+            user: 'Desperado',
+            host: '47.96.95.187',
+            ref: 'origin/master',
+            repo: 'git@github.com:KnowMighT/EnumServer.git',
+            path: '/home/Desperado/BimLink/PM2Deploy',
+            'post-deploy': 'npm install',
+            env: {
+                NODE_ENV: 'dev'
+            }
+        }
     }
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
-  }
 };
