622c25ca039d5106f6dca10ca1ece7b0763733b0
Desperado <731759679@qq.com>
2018-06-21 17:04:38 +0800

    ignore

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
             ref: 'origin/master',
             repo: 'git@github.com:KnowMighT/EnumServer.git',
             path: '/home/Desperado/BimLink/PM2Deploy',
-            'post-deploy': 'npm install'
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.json --env production'
         },
 
         dev: {
