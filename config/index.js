055dcb2910ecaf8417b58d403958b73e0baa90b0
Desperado <731759679@qq.com>
2018-06-21 18:21:32 +0800

    update

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 module.exports = {
     max_body: 500 * 1024 * 1024, // 500M
     // mongodbURL: 'mongodb://bimlink:yizhu2018bimlink@192.168.1.59:37017/bimlinkdb',
-    mongodbURL: 'mongodb://bimlink:yizhu2018bimlink@192.168.1.59:37017/new_bimlinkdb'
+    mongodbURL: 'mongodb://bimlink:yizhu2018bimlink@localhost:37017/new_bimlinkdb'
 
 };
 
